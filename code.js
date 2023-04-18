172de04f751313f2b155fcf301335ea663b66190
codeace110 <neri11032321@gmail.com>
2023-04-18 20:40:09 +0800

    first commit

---
+++ code.js
@@ -0,0 +1,85 @@
+function runCode() {
+  var outputContainer = document.getElementById("output-container");
+  var code = editor.getValue();
+
+  // Clear the output container
+  outputContainer.innerHTML = "";
+
+  // Redirect console.log() to the output container
+  var oldLog = console.log;
+  console.log = function (message) {
+    var pre = document.createElement("pre");
+    pre.innerText = message;
+    outputContainer.appendChild(pre);
+  };
+
+  // Try to run the code and catch any errors
+  try {
+    eval(code);
+  } catch (error) {
+    // If an error occurs, display a custom error message in the output container
+    var errorMessage =
+      "Error: " + error.name + "\nMessage: " + error.message + "\n\n";
+    errorMessage += "Possible fix: ";
+    switch (error.name) {
+      case "SyntaxError":
+        errorMessage +=
+          "Check for syntax errors in your code and correct them.";
+        break;
+      case "ReferenceError":
+        errorMessage +=
+          "Make sure that all variable and function names are spelled correctly and exist in the current scope.";
+        break;
+      case "TypeError":
+        errorMessage +=
+          "Check the data types of your variables and make sure that they are used correctly in your code.";
+        break;
+      default:
+        errorMessage += "Check your code for any errors and try again.";
+    }
+    var p = document.createElement("p");
+    p.innerText = errorMessage;
+    p.style.color = "#FC2947";
+    p.style.fontSize = "17px";
+    p.style.fontWeight = "100";
+    p.style.marginTop = "-30px";
+    p.style.padding = "30px";
+    outputContainer.appendChild(p);
+  }
+
+  // Restore console.log()
+  console.log = oldLog;
+}
+var editor = CodeMirror.fromTextArea(document.getElementById("editor"), {
+  lineNumbers: true,
+  matchBrackets: true,
+  autoCloseBrackets: true,
+  mode: "javascript",
+  theme: "dracula",
+});
+
+// Set the editor width and height
+editor.setSize("100%", "100%");
+
+// Set the output container height
+var outputContainer = document.getElementById("output-container");
+outputContainer.style.height = window.innerHeight - 120 + "px";
+
+// Listen for window resize events and adjust the output container height accordingly
+window.addEventListener("resize", function () {
+  outputContainer.style.height = window.innerHeight - 120 + "px";
+});
+
+// Initialize the editor when the DOM is ready
+document.addEventListener("DOMContentLoaded", function (event) {
+  initEditor();
+});
+
+// Get log and output buttons
+var logButton = document.getElementById("log-button");
+var outputButton = document.getElementById("output-button");
+
+// Add click event listeners to buttons
+logButton.addEventListener("click", toggleOutput);
+outputButton.addEventListener("click", toggleOutput);
+
---
+++ index.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    initEditor();
+});
+
+
+//end of load Dom content
+
+const toggleButton = document.getElementById("theme-toggle");
+
+const body = document.body;
+
+toggleButton.addEventListener("click", function() {
+    body.classList.toggle("dark-theme");
+});;
+
+//end of theme togle
